962d127961dfb1d6d919cc1e46853335787b7444
benjaminParisel <benjamin.parisel@bonitasoft.com>
2021-12-09 14:47:04 +0100

    feat: check diff not contains some string

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# gh-pr-diff-checker
+# Github PR diff checker
+
+This action checks the diff in a PR, and fails if one or more of the set criteria isn't met.
+
+# Using this action
+
+You need to add this in a file in `.github/workflows` and set appropriate options.
+
+```
+name: Check PR content
+
+on: [pull_request]
+
+jobs:
+  check_pr:
+    runs-on: ubuntu-latest
+    name: Check for forbidden string
+    steps:
+      - name: Scan forbidden string
+        uses: benjaminParisel/gh-pr-diff-checker@main
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          diffDoesNotContain: ['http://documentation.mydomain','link:']
+```
+
+An example is also provided in .github/workflows/ in this repository.
+
+## License
+
+This is a modification and inspiration from [JJ/github-pr-contains-action](https://github.com/JJ/github-pr-contains-action/) and is released under the MIT license.
---
+++ src/main.ts
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const github = require('@actions/github')
+const parse = require('parse-diff')
+
+async function run() {
+    try {
+        // get information on everything
+        const token = core.getInput('github-token', { required: true })
+        const octokit = github.getOctokit(token)
+        const context = github.context
+
+        // Request the pull request diff from the GitHub API
+        const { data: prDiff } = await octokit.pulls.get({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            pull_number: context.payload.pull_request.number,
+            mediaType: {
+                format: "diff",
+            },
+        });
+        const files = parse(prDiff)
+
+        // Get changed chunks
+        let changes = ''
+        files.forEach(function(file) {
+            file.chunks.forEach(function(chunk) {
+                chunk.changes.forEach(function(change) {
+                    if (change.add) {
+                        changes += change.content
+                    }
+                })
+            })
+        })
+
+        // Check that the pull request diff does not contain the forbidden string
+        let diffDoesNotContain :Array<string> = core.getInput('diffDoesNotContain')
+
+        if (diffDoesNotContain.length > 0) {
+            diffDoesNotContain.forEach(pattern => {
+                if (changes.includes(pattern)) {
+                    core.setFailed(`The PR diff should not include one of ${diffDoesNotContain.toString()}`);
+                }
+            });
+        }
+
+    } catch (error:any) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
